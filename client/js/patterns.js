6f7376c897dbfa70c2240f7eabc8880c0ce5b199
lzbk <mlois_-gr@yahoo.fr>
2013-09-18 15:35:43 +0200

    weee bit of css

--- client/js/patterns.js
+++ client/js/patterns.js
@@ -11,7 +11,7 @@ Patterns = {
     COMMENTITEM: "{0}: {1} <span class='date'>{2}</span>",
     POSITIONITEM: "<li><span class='date'>{2}</span> {0} ({1})</li>\n",
     CARD: "<article id='{0}'><header><h2>{1}</h2><span>&nbsp;</span></header>"+
-        "\n\t<img src='{2}' />\n\t<p>{3}</p>"+
+        "\n\t<section class='details'><img src='{2}' />\n\t<p>{3}</p></section>"+
         "\n\t<footer>"+
         "\n\t\t<p class='comments'>{4}</p>"+
         "\n\t\t<ul class='position'>{5}</ul>"+
